50655c778fafed3ad6ce3c190c7df9ce4acba412
aryan shrestha <aryanshrestha0981@gmail.com>
2025-06-26 02:08:23 +0545

    employee model and factory

--- database/migrations/2025_06_25_162240_create_tags_table.php
+++ database/migrations/2025_06_25_162240_create_tags_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            //foreign id called employer_id
-//            $table->unsignedBigInteger('employer_id');
-            $table->foreignIdFor(\App\Models\Employer::class);
-            $table->string('title');
-            $table->string('salary');
             $table->timestamps();
         });
     }
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('tags');
     }
 };
